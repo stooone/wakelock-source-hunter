bbbd061f59a43218c9eb47d7fc1c24538e7c35f1
Stone <stone@midway.hu>
2018-10-19 09:37:42 +0200

    Check if the users select an existing wakelock, and if the wakeup_sources file is there.

--- wakelock-source-hunter.sh
+++ wakelock-source-hunter.sh
@@ -8,6 +8,16 @@ WAKELOCK=IPA_WS
 PARENT=$(dumpsys window windows | grep mCurrentFocus | cut -d'{' -f2 |cut -d' ' -f3 |cut -d'/' -f1)
 echo -e "Parent app is ${PARENT}, will skip it during the tests.\n"
 
+if [ ! -e /sys/kernel/debug/wakeup_sources ]; then
+  echo "Error: can't find the wakeup_sources file."
+  exit 1
+fi
+
+if ! grep ${WAKELOCK} /sys/kernel/debug/wakeup_sources >/dev/null; then
+  echo "Error: you have selected the ${WAKELOCK} to hunt down, but your phone doesn't have this wakelock. Please edit this script and select another wakelock."
+  exit 1
+fi
+
 echo -e "I will count ${WAKELOCK} wakelocks during disabling apps one-by-one. But before I'll make some baseline with all apps enabled. Please wait...\n"
 
 for i in $( seq 3 ); do
